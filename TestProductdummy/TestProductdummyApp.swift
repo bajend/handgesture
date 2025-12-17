9446d7f2177bb918946c91560b4abb30c0be34ef
bharath <bharath2005@gmail.com>
2025-12-16 16:21:08 -0800

    Initial Commit

---
+++ TestProductdummy/TestProductdummyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProductdummyApp.swift
+//  TestProductdummy
+//
+//  Created by Ajendla, Bharath on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct TestProductdummyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
